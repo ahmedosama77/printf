e47e707517ef5846f4b0fae15d6c97e095d7f68b
Ahmed osama <ahmed.osama.elfouly@gmail.com>
2023-04-23 09:23:50 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -53,6 +53,9 @@ int _printf(const char *format, ...)
 	return (printed_chars);
 }
 
+
+
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
--- main.h
+++ main.h
@@ -24,6 +24,7 @@
  * @fmt: The format.
  * @fn: The function associated.
  */
+
 struct fmt
 {
 	char fmt;
@@ -37,13 +38,12 @@ struct fmt
  * @fmt: The format.
  * @fm_t: The function associated.
  */
+
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
-
-/****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -106,7 +106,6 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
@@ -114,4 +113,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* MAIN_H */
+#endif
